3a73deeb8224de25343b2f5ffd2fa0d193d6d177
Will-Barnard-WB <barnardwillliam@outlook.com>
2025-06-22 21:28:45 +0100

    centre nav bar

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -18,7 +18,7 @@ export const createCheckoutSession = async (req, res) => {
 
 			return {
 				price_data: {
-					currency: "usd",
+					currency: "gbp",
 					product_data: {
 						name: product.name,
 						images: [product.image],
