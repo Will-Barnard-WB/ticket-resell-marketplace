f141b3080aab2da2a9e0a910b63355d8e5c9bd4e
Will-Barnard-WB <barnardwillliam@outlook.com>
2025-06-23 11:57:53 +0100

    allowing user's to list product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
+
 import CategoryPage from "./pages/CategoryPage";
 
 import Navbar from "./components/Navbar";
@@ -49,6 +50,10 @@ function App() {
 						<Route
 							path='/secret-dashboard'
 							element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}
+						/>
+						<Route
+							path='/add-product'
+							element={user ? <AddProductPage /> : <Navigate to='/login' />}
 						/>
 						<Route path='/category/:category' element={<CategoryPage />} />
 						<Route path='/cart' element={user ? <CartPage /> : <Navigate to='/login' />} />
